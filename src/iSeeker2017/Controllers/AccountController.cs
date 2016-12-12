bec1e074d92de68d5bd5cd67dd2d5a31812c1be1
Brian Caputo <bcaputo1@comcast.net>
2016-12-12 00:02:54 -0500

    base 12/12/2016

---
+++ src/iSeeker2017/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace iSeeker2017.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public SignInResult SignIn()
+        //{
+            
+        //}
+        [Route("SignIn")]
+        [Route("SignOn")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [Route("SignOut")]
+        [Route("SignOff")]
+        public IActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
